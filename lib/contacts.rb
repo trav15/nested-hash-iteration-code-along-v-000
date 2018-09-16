6f045d4e4373da7a00146d48f902990a8d6aac81
Travis T <travistokuyama@gmail.com>
2018-09-16 21:44:20 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,7 +18,7 @@ def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     contact_details_hash.each do |attribute, data|
       if attribute == "favorite_ice_cream_flavors"
-        data.delete_if {|ice_cream_flavor| ice_cream_flavor == "strawberry"
+        data.delete_if {|ice_cream_flavor| ice_cream_flavor == "strawberry"}
       end
     end
   end
